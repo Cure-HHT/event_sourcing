c819545af6e107040c0429a07e48824917603739
Michael Lewis <michael@anspar.org>
2026-05-21 12:13:39 -0700

    [CUR-1317] reaction remote: RemoteActionSubmitter (POST /actions)

---
+++ reaction/lib/src/remote/remote_action_submitter.dart
@@ -0,0 +1,40 @@
+import 'dart:convert';
+
+import 'package:event_sourcing/event_sourcing.dart';
+import 'package:reaction/src/interfaces/action_submitter.dart';
+import 'package:reaction/src/interfaces/auth_session.dart';
+import 'package:reaction/src/remote/remote_auth_session.dart';
+import 'package:reaction/src/remote/remote_connection.dart';
+import 'package:reaction/src/wire/action_submission_codec.dart';
+import 'package:reaction/src/wire/dispatch_result_codec.dart';
+
+class RemoteActionSubmitter implements ActionSubmitter {
+  RemoteActionSubmitter({required this.connection, required this.authSession});
+
+  final RemoteConnection connection;
+  final AuthSession authSession;
+
+  @override
+  Future<DispatchResult<Object?>> submit(ActionSubmission submission) async {
+    if (authSession.current is! Authenticated) {
+      throw const TransportException('not authenticated');
+    }
+    final url = connection.baseUrl.replace(path: '/actions');
+    final res = await connection.httpPost(
+      url,
+      body: jsonEncode(ActionSubmissionCodec.encode(submission)),
+    );
+    if (res.statusCode == 401) {
+      if (authSession is RemoteAuthSession) {
+        (authSession as RemoteAuthSession).onWireUnauthorized();
+      }
+      throw const TransportException('unauthorized');
+    }
+    if (res.statusCode != 200) {
+      throw TransportException('http ${res.statusCode}');
+    }
+    return DispatchResultCodec.decode(
+      jsonDecode(res.body) as Map<String, Object?>,
+    );
+  }
+}
